3495d1a5044408e6aba7319aaea2334eb7aae61f
Aryan Sahni <aryansahni5579@gmail.com>
2026-03-31 22:35:28 -0700

    add

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
+        source: "/api/:path((?!auth).*)",
         destination: `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000"}/api/:path*`,
       },
     ];
